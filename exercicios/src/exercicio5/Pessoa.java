a5a0f88c05482f8a2b3922e97dea43afbad312c2
Larissa <larissa.asito@gmail.com>
2023-02-24 19:59:09 -0300

    Adicionando exercicio 5

---
+++ exercicios/src/exercicio5/Pessoa.java
@@ -0,0 +1,10 @@
+package exercicio5;
+
+public class Pessoa {
+    protected String nome;
+    protected String sobrenome;
+
+    protected String obterNomeCompleto(){
+        return nome + " " + sobrenome;
+    }
+}
